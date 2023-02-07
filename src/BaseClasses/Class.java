f61aaf8a0ad72d57cc5ec724c4564a19503220aa
Steve Carlson <stephen.carl521@gmail.com>
2023-02-07 17:44:19 +0000

    Added level method to each class

--- src/BaseClasses/Class.java
+++ src/BaseClasses/Class.java
@@ -1,10 +1,15 @@
 package BaseClasses;
+
+import java.util.logging.Level;
+
 abstract class Class{
     //private fields
     private int hitDie;
     private int level;
     //public fields
+    //Constructor
     public Class(int hd){hitDie = hd;}
+    //methods
     public abstract void level();
     public void setHidDie(int h){hitdie = h;}
     public int getHitDie(){return hitDie;}
@@ -15,71 +20,89 @@ abstract class Class{
 class Artificer extends Class{
         //private fields
         //public fields
-                public Artificer(){super();}
+        public Artificer(){super(8);}
+        public level(){}
         }
 
 class Barbarian extends Class{
         //private fields
         //public fields
-                public Barbarian(){super(12);}
+        public Barbarian(){super(12);}
+        public level(){}
         }
 class Bard extends Class{
-//private fields
-//public fields
-        public Bard(){super(8);}
+        //private fields
+        int inspirationDie;
+        //public fields
+        public Bard(){
+                super(8);
+                inspirationDie = 6;
+        }
+        public level(){}
 }
 class BloodHunter extends Class{
 //private fields
 //public fields
-        public BloodHunter(){super();}
+        public BloodHunter(){super(10);}
+        public level(){}
 }
 class Cleric extends Class{
         //private fields
         //public fields
-                public Cleric(){super(8);}
+        public Cleric(){super(8);}
+        public level(){}
         }
 class Druid extends Class{
 //private fields
 //public fields
         public Druid(){super(8);}
+        public level(){}
 }
 class Fighter extends Class{
 //private fields
 //public fields
         public Fighter(){super(10);}
+        public level(){}
 }
 class Monk extends Class{
 //private fields
 //public fields
         public Monk(){super(8);}
+        public level(){}
 }
 class Paladin extends Class{
         //private fields
         //public fields
-                public Paladin(){super(10);}
+        public Paladin(){super(10);}
+        public level(){}
         }
 class Ranger extends Class{
         //private fields
         //public fields
-                public Ranger(){super(10);}
+        public Ranger(){super(10);}
+        public level(){}
         }
 class Rogue extends Class{
 //private fields
 //public fields
         public Rogue(){super(8);}
+        public level(){}
 }
 class Sorcerer extends Class{
         //private fields
         //public fields
-                public Sorcerer(){super(6);}
+        public Sorcerer(){super(6);}
+        public level(){}
         }
 class Warlock extends Class{
 //private fields
 //public fields
         public Warlock(){super(8);}
+        public level(){}
 }
 class Wizard extends Class{
         //private fields
         //public fields
-                public Wizard(){super(6);}
+        public Wizard(){super(6);}
+        public level(){}
         }
