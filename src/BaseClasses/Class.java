510848b6e2ba94133582757c01b6d99d96657bba
Steve Carlson <stephen.carl521@gmail.com>
2023-02-07 21:09:54 +0000

    fixing abstract interface complaince

--- src/BaseClasses/Class.java
+++ src/BaseClasses/Class.java
@@ -1,6 +1,6 @@
 package BaseClasses;
 
-abstract class PlayerPlayerClass{
+abstract class PlayerClass{
     //private fields
     private int hitDie;
     private int level;
@@ -9,7 +9,7 @@ abstract class PlayerPlayerClass{
     public PlayerClass(int hd){hitDie = hd;}
     //methods - Getters & Setters
     public abstract void level();
-    public void setHidDie(int h){hitdie = h;}
+    public void setHidDie(int h){hitDie = h;}
     public int getHitDie(){return hitDie;}
     public void setLevel(int l){level = l;}
     public int getLevel(){return level;}
@@ -20,14 +20,14 @@ class Artificer extends PlayerClass{
         //private fields
         //public fields
         public Artificer(){super(8);}
-        public level(){}
+        public void level(){}
         }
 
 class Barbarian extends PlayerClass{
         //private fields
         //public fields
         public Barbarian(){super(12);}
-        public level(){}
+        public void level(){}
         }
 class Bard extends PlayerClass{
         //private fields
@@ -37,71 +37,71 @@ class Bard extends PlayerClass{
                 super(8);
                 inspirationDie = 6;
         }
-        public level(){}
+        public void level(){}
 }
 class BloodHunter extends PlayerClass{
 //private fields
 //public fields
         public BloodHunter(){super(10);}
-        public level(){}
+        public void level(){}
 }
 class Cleric extends PlayerClass{
         //private fields
         //public fields
         public Cleric(){super(8);}
-        public level(){}
+        public void level(){}
         }
 class Druid extends PlayerClass{
 //private fields
 //public fields
         public Druid(){super(8);}
-        public level(){}
+        public void level(){}
 }
 class Fighter extends PlayerClass{
 //private fields
 //public fields
         public Fighter(){super(10);}
-        public level(){}
+        public void level(){}
 }
 class Monk extends PlayerClass{
 //private fields
 //public fields
         public Monk(){super(8);}
-        public level(){}
+        public void level(){}
 }
 class Paladin extends PlayerClass{
         //private fields
         //public fields
         public Paladin(){super(10);}
-        public level(){}
+        public void level(){}
         }
 class Ranger extends PlayerClass{
         //private fields
         //public fields
         public Ranger(){super(10);}
-        public level(){}
+        public void level(){}
         }
 class Rogue extends PlayerClass{
 //private fields
 //public fields
         public Rogue(){super(8);}
-        public level(){}
+        public void level(){}
 }
 class Sorcerer extends PlayerClass{
         //private fields
         //public fields
         public Sorcerer(){super(6);}
-        public level(){}
+        public void level(){}
         }
 class Warlock extends PlayerClass{
 //private fields
 //public fields
         public Warlock(){super(8);}
-        public level(){}
+        public void level(){}
 }
 class Wizard extends PlayerClass{
         //private fields
         //public fields
         public Wizard(){super(6);}
-        public level(){}
+        public void level(){}
         }
